a8e2d035e476465c49571915704ef1ef411809ab
Armaan C <armaanchandarana1@gmail.com>
2026-04-01 16:57:21 -0400

    fixed minor issue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,18 +4,6 @@
       <h3>EMR Dev Menu</h3>
       <div class="nav-links">
         <router-link to="/" class="nav-btn">1. Login</router-link>
-        <br>
-        <router-link to="/dashboard" class="nav-btn">2. Dashboard</router-link>
-        <br>
-        <router-link to="/directory" class="nav-btn">3. Directory</router-link>
-        <br>
-        <router-link to="/record" class="nav-btn">4. Record</router-link>
-        <br>
-        <router-link to="/consultation" class="nav-btn">5. Consultation</router-link>
-        <br>
-        <router-link to="/prescriptions" class="nav-btn">6. Prescriptions</router-link>
-        <br>
-        <router-link to="/scheduling" class="nav-btn">7. Scheduling</router-link>
       </div>
     </nav>
 
