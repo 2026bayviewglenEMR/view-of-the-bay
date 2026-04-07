6b13c433168ea4aa657aaed15186241266cbf1ba
Armaan C <armaanchandarana1@gmail.com>
2026-04-07 13:48:36 -0400

    added element plus and made basic login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <nav class="dev-navigation">
       <h3>EMR Dev Menu</h3>
       <div class="nav-links">
-        <router-link to="/" class="nav-btn">1. Login</router-link>
+        <router-link to="/login" class="nav-btn">1. Login</router-link>
       </div>
     </nav>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/login',
       name: 'login',
       component: () => import('../views/Login.vue') 
     },
