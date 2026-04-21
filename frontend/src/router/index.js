bbfd62ac9e68e5cdcee3bccda1dc16434d575243
Riya Varia <rvaria@bayviewglen.ca>
2026-04-21 08:44:24 -0400

    Revert "Merge branch 'main' of https://github.com/2026bayviewglenEMR/view-of-the-bay"
    This reverts commit fce3be9edf17ff433e150035524f4207d6bec5c5, reversing
    changes made to f0d8e56a722231cfc4180bd1c43ae1d38a2b639f.
    
    update routes#

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,19 +24,9 @@ const router = createRouter({
       component: () => import('../views/Templates.vue')
     },
     {
-      path: '/template/:patientId',
-      name: 'templateUse',
-      component: () => import('../views/TemplateUse.vue')
-    },
-    {
       path: '/messaging',
       name: 'messaging',
       component: () => import('../views/Messaging.vue')
-    },
-    {
-      path: '/waitingroom',
-      name: 'waitingroom',
-      component: () => import('../views/WaitingRoom.vue')
     }
   ]
 })
@@ -46,13 +36,13 @@ const openRoutes = [
   '/login'
 ]
 
-// router.beforeEach((to, from, next) => {
-//   //if the route requires a token, go to login
-//   if (localStorage.getItem("token") || openRoutes.includes(to.path)) {
-//     next();
-//   } else {
-//     next('/login');
-//   }
-// });
+router.beforeEach((to, from, next) => {
+  //if the route requires a token, go to login
+  if (localStorage.getItem("token") || openRoutes.includes(to.path)) {
+    next();
+  } else {
+    next('/login');
+  }
+});
 
 export default router
