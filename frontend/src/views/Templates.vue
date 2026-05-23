7a721b49f91524939ddab84e21bdb0f2c9bb2c07
Riya Varia <rvaria@bayviewglen.ca>
2026-05-23 18:43:00 -0400

    add sidebar to templates page

--- frontend/src/views/Templates.vue
+++ frontend/src/views/Templates.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
-    <h1>Select Template</h1>
+  <MainLayout>
+    <div>
+      <h1>Select Template</h1>
 
-    <ul>
-      <li v-for="t in templates" :key="t.name">
-        <button @click="selectTemplate(t)">
-          {{ t.name }}
-        </button>
-      </li>
-    </ul>
+      <ul>
+        <li v-for="t in templates" :key="t.name">
+          <button @click="selectTemplate(t)">
+            {{ t.name }}
+          </button>
+        </li>
+      </ul>
 
-    <TemplateRenderer
-      v-if="selectedTemplate"
-      :template="selectedTemplate"
-    />
-  </div>
+      <TemplateRenderer
+        v-if="selectedTemplate"
+        :template="selectedTemplate"
+      />
+    </div>
+  </MainLayout>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { getTemplates } from "@/templates/templateSystem";
 import TemplateRenderer from "@/components/templates/TemplateRenderer.vue";
+import MainLayout from '../components/MainLayout.vue'
 
 const templates = getTemplates();
 const selectedTemplate = ref(null);
